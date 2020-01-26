f9274265c1484731d2e8dbd21d688735de792f15
Tobi Weinstock <tv.weinstock@gmail.com>
2020-01-26 18:48:25 +0100

    printing gym names

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDom from 'react-dom';
 import algoliasearch from 'algoliasearch';
+import makeCancelable from '../Utils/makeCancellable';
 
 interface GeoLoc {
   lat: number;
@@ -19,33 +20,53 @@ interface Gym {
   _geoloc: GeoLoc;
 }
 
-const App = () => {
+// Create one regular function that does the call and return a promise of hits
+const getHits = query => {
   const appID: string = process.env.ALGOLIA_APP_ID!;
   const apiKey: string = process.env.ALGOLIA_SEARCH_API_KEY!;
   const indexName: string = process.env.ALGOLIA_INDEX_NAME!;
-
   const searchClient = algoliasearch(appID, apiKey);
-  const index = searchClient.initIndex(indexName);
+  const algoliaIndex = searchClient.initIndex(indexName);
 
-  // let gymHits: Gym[] = [];
+  const [results, setResults] = useState<any[]>([]);
 
-  index
-    .search({ query: 'yoga' })
-    .then(({ hits } = {}) => {
-      // gymHits.push(hits);
-      // console.log(gymHits);
-      return hits;
-    })
-    .catch(err => {
-      console.log(err);
-      console.log(err.debugData);
-    });
-  console.log(hits);
+  useEffect(() => {
+    const searchPromise = makeCancelable(
+      algoliaIndex.search({ query, hitsPerPage: 100 })
+    );
+    searchPromise.then(result => setResults(result.hits));
+    return () => {
+      searchPromise.cancel();
+    };
+  }, [query]);
+  return results;
+};
+
+// Create a hook that calls that function, triggers the search with a useEffect and stores the gyms using useState
+
+const App = () => {
+  const [gyms, setGyms] = useState(0);
+  const gymList: Gym[] = getHits('');
+
+  useEffect(() => {
+    setGyms(gymList);
+  });
+
   return (
     <React.Fragment>
-      <div>Lol</div>
+      {gymList.length > 0 ? (
+        <ul>
+          {gymList.map(gym => (
+            <li key={gym.objectID}>{gym.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <div>pas de gyms</div>
+      )}
     </React.Fragment>
   );
 };
 
 const Root = () => <App />;
+
+ReactDom.render(<Root />, document.getElementById('root'));
